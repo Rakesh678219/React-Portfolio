6b97cc5d4cb694c8a3739f11717a7ff90c5880d6
rakesh <rpeddamallu@juniper.net>
2022-09-10 15:29:49 +0530

    prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
     // "plugin:import/errors",
     // "plugin:import/warnings",
     // "plugin:import/typescript",
@@ -20,14 +20,16 @@ module.exports = {
     // "plugin:eslint-comments/recommended",
     // "prettier/@typescript-eslint",
     // "plugin:prettier/recommended",
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "@typescript-eslint/no-var-requires": "off",
-    "react/prop-types": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import './styles.css';
+import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
-import { ClickCounter } from './ClickCounter';
+import { ClickCounter } from './ClickCounter'
 export const App = () => {
-     return (
-          <>
-               <h1> React Portfolio - {process.env.NODE_ENV}  - {process.env.name}</h1>
-               <img src={IMAGE} alt="React logo" width="300" height="300" />
-               <img src={LOGO} alt="React logo" width="300" />
-               <ClickCounter />
-          </>
-     )
+  return (
+    <>
+      <h1>
+        {' '}
+        React Portfolio - {process.env.NODE_ENV} - {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React logo" width="300" height="300" />
+      <img src={LOGO} alt="React logo" width="300" />
+      <ClickCounter />
+    </>
+  )
 }
-
