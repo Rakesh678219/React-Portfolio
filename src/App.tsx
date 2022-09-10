3f63c2350bc89075778636f3be5ff1d4c074d8b5
rakesh <rpeddamallu@juniper.net>
2022-09-10 15:44:07 +0530

    Done with setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import './styles.css'
-import IMAGE from './react.png'
-import LOGO from './logo.svg'
-import { ClickCounter } from './ClickCounter'
 export const App = () => {
-  const name = 'Rakesh'
   return (
     <>
-      <h1>
-        {' '}
-        React Portfolio {name} - {process.env.NODE_ENV} - {process.env.name}
-      </h1>
-      <img src={IMAGE} alt="React logo" width="300" height="300" />
-      <img src={LOGO} alt="React logo" width="300" />
-      <ClickCounter />
+      <h1>Hello World</h1>
     </>
   )
 }
