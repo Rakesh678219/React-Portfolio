f778a7216ebab1ee68a26e0c03fec7aa5cf5c88a
rakesh <rpeddamallu@juniper.net>
2022-09-10 15:37:49 +0530

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import IMAGE from './react.png'
 import LOGO from './logo.svg'
 import { ClickCounter } from './ClickCounter'
 export const App = () => {
+  const name = 'Rakesh'
   return (
     <>
       <h1>
         {' '}
-        React Portfolio - {process.env.NODE_ENV} - {process.env.name}
+        React Portfolio {name} - {process.env.NODE_ENV} - {process.env.name}
       </h1>
       <img src={IMAGE} alt="React logo" width="300" height="300" />
       <img src={LOGO} alt="React logo" width="300" />
