fcdb156e24c293a3378b4c59f1544f41a93fcdd6
rakesh <rpeddamallu@juniper.net>
2022-09-10 14:56:54 +0530

    changes of webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './styles.css';
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter';
 export const App = () => {
      return (
-          <h1>React Portfolio</h1>
+          <>
+               <h1> React Portfolio - {process.env.NODE_ENV}  - {process.env.name}</h1>
+               <img src={IMAGE} alt="React logo" width="300" height="300" />
+               <img src={LOGO} alt="React logo" width="300" />
+               <ClickCounter />
+          </>
      )
 }
 
