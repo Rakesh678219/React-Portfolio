48abafeb8429aa7fdd6b029b2690f30a6270a12a
rakesh <rpeddamallu@juniper.net>
2022-09-26 09:44:43 +0530

    Responsive Nav Bar

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -3,36 +3,28 @@ import './Navbar.css'
 import '../App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-
+function myFunction() {
+     var x = document.getElementById("myTopnav") as HTMLInputElement;
+     if (x.className === "topnav") {
+          x.className += " responsive";
+     } else {
+          x.className = "topnav";
+     }
+}
 const Navbar = () => {
      return (
-          <nav className="nav-bar">
-               <div className="nav-name">
-                    <a href="/">Rakesh Peddamallu</a>
-               </div>
-               <Router>
-                    <ul className='navbar-nav'>
-                         <li>
-                              <a href="/portfolio">Portfolio</a>
-                         </li>
-                         <li>
-                              <a href="/about">About</a>
-                         </li>
-                         <li>
-                              <a href="/projects">Projects</a>
-                         </li>
-                         <li>
-                              <a href="/resume">Resume</a>
-                         </li>
-                         <li>
-                              <a href="/blog">Blog</a>
-                         </li>
-                         <li>
-                              <a href="/contact">Contact</a>
-                         </li>
-                    </ul>
-               </Router>
-          </nav >
+          <div className="topnav" id="myTopnav">
+               <a href="/" className="active">Rakesh Peddamallu</a>
+               <a href="/portfolio">Portfolio</a>
+               <a href="/about">About</a>
+               <a href="/projects">Projects</a>
+               <a href="/resume">Resume</a>
+               <a href="/blog">Blog</a>
+               <a href="/contact">Contact</a>
+               <a href="javascript:void(0);" className="icon" onClick={myFunction}>
+                    <i className="fa fa-bars"></i>
+               </a>
+          </div>
      )
 }
 
