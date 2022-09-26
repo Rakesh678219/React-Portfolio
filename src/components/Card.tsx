b27c86f966ab8e4abb47eb4aa310273ab841dad6
rakesh <rpeddamallu@juniper.net>
2022-09-26 14:14:51 +0530

    css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Button from './Button'
 import './Card.css'
+
+function URL(url: any) {
+     if (url) {
+          location.href = url;
+     }
+}
+
+
 const Card = (props: any) => {
      return (
           <>
                <div className="card-container">
-                    <h1>{props.title}</h1>
+                    <h3>{props.title}</h3>
                     <img width="100%" src={props.img} alt="" />
                     <p>{props.content}</p>
-                    <Button title={'open'} />
+                    <Button title={'open'} onClick={props.onClick} />
                </div>
           </>
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Card from '../components/Card'
 import './Projects.css'
+const hadleClick = (url: any) => {
+     location.href = url;
+}
 const Projects = () => {
      return (
           <>
                <ul className="auto-grid">
-                    <li> <Card title={'Project1'} img={'/bg1.jpg'} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
+                    <li onClick={() => hadleClick('https://sleepy-fjord-15164.herokuapp.com/')} > <Card title={'Weather App'} img={'/weather-app-project.png'} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
                     <li> <Card title={'Project2'} img={'/bg-3.jpg'} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
                     <li> <Card title={'Project3'} img={'/bg-4.jpg'} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
                     <li> <Card title={'Project4'} img={'/bg-10.jpg'} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
