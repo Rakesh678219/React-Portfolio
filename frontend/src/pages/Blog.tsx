3e23b52495864f008766620cafa8e58c3450eef5
rakesh <rpeddamallu@juniper.net>
2022-10-15 19:19:54 +0530

    blogcard issue

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -3,17 +3,18 @@ import BlogCard from '../components/BlogCard'
 import Card from '../components/Card'
 import './Blog.css'
 const Blog = () => {
+     const text = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil harum corporis eius nesciunt labore pariatur non velit sequi. Maxime quo quis dolorum exercitationem architecto consequatur accusantium assumenda? Quae, quam necessitatibus?'
      return (
           <>
                <div className='blog-page'>
                     <h1 className='heading'>From the blog</h1>
                     <ul className="auto-grid-blog">
-                         <li> <BlogCard title={'Blog 1'} img={require('../images/test.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} createdBy={'rakesh peddamallu'} createdAt={'October 25, 2018'} /></li>
-                         <li> <BlogCard title={'Blog 2'} img={require('../images/bg-7.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
-                         <li> <BlogCard title={'Blog 3'} img={require('../images/bg-11.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
-                         <li> <BlogCard title={'Blog 4'} img={require('../images/bg-3.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
-                         <li> <BlogCard title={'Blog 5'} img={require('../images/bg-5.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
-                         <li> <BlogCard title={'Blog 6'} img={require('../images/bg-7.jpg')} content={'Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit fugiat iste repellendus nesciunt culpa veritatis earum maxime, eum assumenda quisquam. ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, harum! Odit, assumenda maxime ab eaque inventore cupiditate non nam recusandae eligendi blanditiis aliquam officia architecto?'} /></li>
+                         <li> <BlogCard title={'Blog 1'} img={require('../images/test.jpg')} content={text} createdBy={'rakesh peddamallu'} createdAt={'October 25, 2018'} /></li>
+                         <li> <BlogCard title={'Blog 2'} img={require('../images/bg-7.jpg')} content={text} /></li>
+                         <li> <BlogCard title={'Blog 3'} img={require('../images/bg-11.jpg')} content={text} /></li>
+                         <li> <BlogCard title={'Blog 4'} img={require('../images/bg-3.jpg')} content={text} /></li>
+                         <li> <BlogCard title={'Blog 5'} img={require('../images/bg-5.jpg')} content={text} /></li>
+                         <li> <BlogCard title={'Blog 6'} img={require('../images/bg-7.jpg')} content={text} /></li>
                     </ul>
                </div>
           </>
