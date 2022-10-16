10d4970bfed62d2bd6bd29f32a718999a452eb40
rakesh <rpeddamallu@juniper.net>
2022-10-16 11:07:45 +0530

    bars

--- frontend/src/Navbar/Navbar.tsx
+++ frontend/src/Navbar/Navbar.tsx
@@ -21,6 +21,8 @@ const Navbar = () => {
                     <Link to="/resume"> Resume</Link>
                     <Link to="/blog">Blog</Link>
                     <Link to="/contact">Contact</Link>
+                    <Link to="javascript:void(0);" className="icon" onClick={myFunction}>
+                         <i className="fa fa-bars"></i> </Link>
                </nav>
           </div>
      )
