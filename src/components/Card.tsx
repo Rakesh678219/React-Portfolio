97082a85b0dd204f4444332e154750c6d4fc27a9
Rakesh Reddy Peddamallu <rpeddamallu@juniper.net>
2023-04-01 15:46:30 +0530

    some css changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,9 +13,9 @@ const Card = (props: any) => {
      return (
           <>
                <div className="card-container">
-                    <h3 className="sub-heading">{props.title}</h3>
-                    <img width="100%" src={props.img} alt="" />
-                    <p className='text-content'>{props.content}</p>
+                    <h3 style={{ color: '#fff' }} className="sub-heading">{props.title}</h3>
+                    <img style={{ borderRadius: '10px' }} width="100%" src={props.img} alt="" />
+                    <p style={{ color: '#fff' }} className='text-content'>{props.content.substring(0, 120) + '...'}</p>
                     <Button title={'open'} onClick={props.onClick} />
                </div>
           </>
