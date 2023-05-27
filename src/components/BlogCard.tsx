630d4d84861a90f4ea288e47766727c379dba0e6
rpeddamallu <rpeddamallu@juniper.net>
2023-05-27 12:51:02 +0530

    background change

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -5,9 +5,9 @@ const BlogCard = (props: any) => {
      return (
           <div className="blog-card-container">
                <img width="100%" src={props.img} alt="" />
-               <small>By <strong className="created-by">{props.createdBy}</strong> | {props.createdAt}</small>
+               <small style={{color:"white"}}>By <strong className="created-by">{props.createdBy}</strong> | {props.createdAt}</small>
                <h3 className="sub-heading">{props.title}</h3>
-               <p className="text-content">{props.content}</p>
+               <p className="text-content">{props.content.substring(0, 200)+'...'}</p>
                <Button title={'Read more...'} />
           </div>
      )
